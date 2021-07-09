2a079389e22f4936e9eea11b917a79db2cddadf4
Shubham Pandey <31141840+Shubham-253@users.noreply.github.com>
2021-07-09 09:24:00 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"rahul2699@yahoo.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
